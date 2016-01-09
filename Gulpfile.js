11dd496a93fec75920a81fe5e4bcbb3dce62dc78
Federico Knüssel <fknussel@gmail.com>
2016-01-09 23:23:25 +1100

    Update Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,13 @@
 const gulp = require('gulp');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
 const htmlmin = require('gulp-htmlmin');
 const del = require('del');
-const gutil = require('gulp-util');
-const source = require('vinyl-source-stream');
-const browserify = require('browserify');
-const watchify = require('watchify');
-// const babelify = require('babelify');
-const reactify = require('reactify');
-
-gulp.task('clean', () => {
-  return del([
-    'dist/**/*'
-  ]);
-});
+const webserver = require('gulp-webserver');
 
 gulp.task('html', () => {
   return gulp.src('src/{,**/}*.html')
@@ -26,41 +18,49 @@ gulp.task('html', () => {
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('css', () => {
-  return gulp.src('src/css/app.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
+gulp.task('bundle', () => {
+  return browserify({
+      entries: 'src/js/app.jsx',
+      extensions: ['.jsx'],
+      debug: true
+    })
+    .transform('babelify', { presets: ['es2015', 'react'] })
+    .bundle()
+    .on('error', function (err) {
+      console.error(err);
+      this.emit('end');
+    })
+    .pipe(source('app.min.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('sass', () => {
+  gulp.src('src/css/app.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('js', () => {
-  var bundler = watchify(browserify({
-    entries: ['src/js/app.jsx'],
-    transform: [reactify],
-    extensions: ['.jsx'],
-    debug: true,
-    cache: {},
-    packageCache: {},
-    fullPaths: true
-  }));
-
-  var build = function (file) {
-    if (file) gutil.log('Recompiling ' + file);
-    return bundler
-      .bundle()
-      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-      .pipe(source('bundle.js'))
-      .pipe(gulp.dest('dist/js'));
-  };
-
-  build();
-  bundler.on('update', build);
+gulp.task('webserver', () => {
+  gulp.src('dist')
+    .pipe(webserver({
+      livereload: true,
+      port: 6789
+    }));
 });
 
-gulp.task('build', ['clean', 'html', 'css', 'js']);
-gulp.task('default', ['build']);
+gulp.task('clean', () => {
+  return del([
+    'dist/**/*'
+  ]);
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/**/*.jsx', ['bundle']);
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('src/**/*.html', ['html']);
+});
+
+gulp.task('build', ['clean', 'bundle', 'sass', 'html']);
+gulp.task('default', ['build', 'webserver', 'watch']);
